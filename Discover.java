944b036db6d06c723a6df67006a0143a77d7f51d
vaibhavmadan96 <vaibhavmadan1996@gmail.com>
2016-03-28 23:35:20 +0530

    discover readded

--- Discover.java
+++ Discover.java
@@ -1,8 +1,11 @@
 package com.example.prakhargautam.moviegenie;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -17,8 +20,9 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class Discover extends Activity {
+    private String entertain;
     ListView lv;
-    MovieListAdapter adapterlv;
+    //MovieListAdapter adapterlv;
     private static String yr;
    private static String pop;
     ArrayAdapter<CharSequence> adapter,adapter1;
@@ -26,6 +30,15 @@ public class Discover extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover);
+        Intent intent=getIntent();
+        if(intent.getStringExtra("entertain")==null)
+        {
+            entertain=new String("movie");
+        }
+        else
+        {
+            entertain=intent.getStringExtra("entertain");
+        }
         yr=new String("2015");
         pop=new String("popularity.desc");
 
@@ -53,30 +66,14 @@ public class Discover extends Activity {
                 {
                     pop="popularity.desc";
                 }
-
-                Call<MovieList> call=ApiClientforDiscover.getInterface().discoverMovies("4cb5dafa2b58b941dff2c731fb26b8c7"
-                        ,pop,Integer.parseInt(yr));
-                call.enqueue(new Callback<MovieList>() {
-                    @Override
-                    public void onResponse(Call<MovieList> call, Response<MovieList> response) {
-                        if (response.isSuccessful()) {
-                            MovieList mv=response.body();
-                            ArrayList<Movie> al=mv.getResults();
-                            adapterlv=new MovieListAdapter(Discover.this,al);
-                            lv.setAdapter(adapterlv);
-
-                        } else {
-                            Toast.makeText(Discover.this,
-                                    response.message() + response.code(), Toast.LENGTH_LONG).show();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<MovieList> call, Throwable t) {
-                        Toast.makeText(Discover.this,
-                                "On failure", Toast.LENGTH_LONG).show();
-                    }
-                });
+                if(entertain.equals("movie"))
+                {
+                    movieDisplay();
+                }
+                else
+                {
+                    tvDisplay();
+                }
             }
 
             @Override
@@ -108,29 +105,14 @@ public class Discover extends Activity {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 pop=parent.getItemAtPosition(position).toString();
-                Call<MovieList> call = ApiClientforDiscover.getInterface().discoverMovies("4cb5dafa2b58b941dff2c731fb26b8c7"
-                        , pop, Integer.parseInt(yr));
-                call.enqueue(new Callback<MovieList>() {
-                    @Override
-                    public void onResponse(Call<MovieList> call, Response<MovieList> response) {
-                        if (response.isSuccessful()) {
-                            MovieList mv = response.body();
-                            ArrayList<Movie> al = mv.getResults();
-                            adapterlv = new MovieListAdapter(Discover.this, al);
-                            lv.setAdapter(adapterlv);
-
-                        } else {
-                            Toast.makeText(Discover.this,
-                                    response.message() + response.code(), Toast.LENGTH_LONG).show();
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(Call<MovieList> call, Throwable t) {
-                        Toast.makeText(Discover.this,
-                                "On failure", Toast.LENGTH_LONG).show();
-                    }
-                });
+                if(entertain.equals("movie"))
+                {
+                    movieDisplay();
+                }
+                else
+                {
+                    tvDisplay();
+                }
             }
 
             @Override
@@ -141,6 +123,17 @@ public class Discover extends Activity {
 
 
                 lv = (ListView) findViewById(R.id.lv);
+        if(entertain.equals("movie"))
+        {
+            movieDisplay();
+        }
+        else
+        {
+            tvDisplay();
+        }
+    }
+    public void movieDisplay()
+    {
         Call<MovieList> call=ApiClientforDiscover.getInterface().discoverMovies("4cb5dafa2b58b941dff2c731fb26b8c7"
                 ,pop,Integer.parseInt(yr));
         call.enqueue(new Callback<MovieList>() {
@@ -149,7 +142,7 @@ public class Discover extends Activity {
                 if (response.isSuccessful()) {
                     MovieList mv = response.body();
                     ArrayList<Movie> al = mv.getResults();
-                    adapterlv = new MovieListAdapter(Discover.this, al);
+                    MovieListAdapter adapterlv = new MovieListAdapter(Discover.this, al);
                     lv.setAdapter(adapterlv);
 //                    Movie movie=al.get(1);
 
@@ -171,5 +164,68 @@ public class Discover extends Activity {
             }
         });
     }
+    public void tvDisplay()
+    {
+        Call<TVList> call=ApiClientforDiscover.getInterface().discoverTV("4cb5dafa2b58b941dff2c731fb26b8c7"
+                , pop, Integer.parseInt(yr));
+        call.enqueue(new Callback<TVList>() {
+            @Override
+            public void onResponse(Call<TVList> call, Response<TVList> response) {
+                if (response.isSuccessful()) {
+                    TVList mv = response.body();
+                    ArrayList<TV> al = mv.getResults();
+                    TVListadapter adapterlv = new TVListadapter(Discover.this, al);
+                    lv.setAdapter(adapterlv);
+//                    Movie movie=al.get(1);
 
+                    //work to be done   put to listview
+                    // + image retreive thing
+                    // +menus
+
+
+                } else {
+                    Toast.makeText(Discover.this,
+                            response.message() + response.code(), Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<TVList> call, Throwable t) {
+                Toast.makeText(Discover.this,
+                        "On failure", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_resource,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId()==R.id.movie)
+        {
+            if(!entertain.equals("movie"))
+            {
+                Intent intent=new Intent();
+                intent.setClass(Discover.this,Discover.class);
+                intent.putExtra("entertain", "movie");
+                startActivity(intent);
+            }
+
+        }
+        else if(item.getItemId()==R.id.tv)
+        {
+            if(entertain.equals("movie"))
+            {
+                Intent intent=new Intent();
+                intent.setClass(Discover.this,Discover.class);
+                intent.putExtra("entertain","tv");
+                startActivity(intent);
+            }
+        }
+        return true;
+    }
 }
